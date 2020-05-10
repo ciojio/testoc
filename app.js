b9be575618984064300d09089ab51ce111cda229
ciojio <ciojio@gmail.com>
2020-05-10 22:20:13 +0630

    updated

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express();
 //console.log(app);
 var http = require('http');
 const server=http.createServer(app);
-app.get('/',(req,res)=>{
-    res.send('Hello ? my world');
-});
+
 const PORT = 8080;
 const HOST = '0.0.0.0';
 //const HOST = 'localhost';
@@ -20,31 +18,17 @@ const HOST = '0.0.0.0';
 app.listen(PORT,HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
 
-getDockerHost = require('get-docker-host');
-isInDocker = require('is-in-docker');
 
-checkDocker = () => {
-    return new Promise((resolve, reject) => {
-        if (isInDocker()) {
-            getDockerHost((error, result) => {
-                if (result) {
-                    resolve(result);
-                } else {
-                    reject(error);
-                }
-            });
-        } else {
-            resolve(null);
-        }
-    });
-};
+const dns = require('dns')
 
-checkDocker().then((addr) => {
-    if (addr) {
-        console.log('Docker host is ' + addr);
-    } else {
-        console.log('Not in Docker');
-    }
-}).catch((error) => {
-    console.log('Could not find Docker host: ' + error);
+var net = dns.lookup('localhost', function(err, result) {
+  console.log(result)
+})
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send(net);
 });
