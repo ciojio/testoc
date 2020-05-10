e776591b5e19798c62a238bd7059288cad86dabd
ciojio <ciojio@gmail.com>
2020-05-10 21:34:40 +0630

    updated

--- app.js
+++ app.js
@@ -19,3 +19,32 @@ const HOST = 'localhost';
 
 app.listen(PORT,HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+getDockerHost = require('get-docker-host');
+isInDocker = require('is-in-docker');
+
+checkDocker = () => {
+    return new Promise((resolve, reject) => {
+        if (isInDocker()) {
+            getDockerHost((error, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+};
+
+checkDocker().then((addr) => {
+    if (addr) {
+        console.log('Docker host is ' + addr);
+    } else {
+        console.log('Not in Docker');
+    }
+}).catch((error) => {
+    console.log('Could not find Docker host: ' + error);
+});
